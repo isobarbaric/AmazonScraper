ed218706b09f5fbcb947fd7db63a3dfb6dacbdf1
Krish Shah <shahkrish2016@gmail.com>
2024-04-27 22:36:20 -0400

    cleaned up scraper code, bundled into its own, neat class

--- amazon/amazon.py
+++ amazon/amazon.py
@@ -1,126 +1,127 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import time
 
-AMAZON_SEARCH_URL = 'https://www.amazon.ca/s?k='
-AMAZON_REVIEW_URL = 'https://www.amazon.ca/product-reviews/'
+class AmazonScraper:
+    __wait_time = 0.5
 
-# all start at page 1
-star_page_suffix = {
-    5: '/ref=cm_cr_unknown?filterByStar=five_star&pageNumber=',
-    4: '/ref=cm_cr_unknown?filterByStar=four_star&pageNumber=',
-    3: '/ref=cm_cr_unknown?filterByStar=three_star&pageNumber=',
-    2: '/ref=cm_cr_unknown?filterByStar=two_star&pageNumber=',
-    1: '/ref=cm_cr_unknown?filterByStar=one_star&pageNumber=',
-}
+    __amazon_search_url = 'https://www.amazon.ca/s?k='
+    __amazon_review_url = 'https://www.amazon.ca/product-reviews/'
 
-def get_amazon_search_page(search_query: str):
-    url = AMAZON_SEARCH_URL + '+'.join(search_query.split())
+    __star_page_suffix = {
+        5: '/ref=cm_cr_unknown?filterByStar=five_star&pageNumber=',
+        4: '/ref=cm_cr_unknown?filterByStar=four_star&pageNumber=',
+        3: '/ref=cm_cr_unknown?filterByStar=three_star&pageNumber=',
+        2: '/ref=cm_cr_unknown?filterByStar=two_star&pageNumber=',
+        1: '/ref=cm_cr_unknown?filterByStar=one_star&pageNumber=',
+    }
 
-    driver = webdriver.Chrome()
-    driver.get(url)
-    driver.implicitly_wait(3)
+    def __init__(self):
+        pass
 
-    html_page = driver.page_source
-    driver.quit()
+    def __get_amazon_search_page(self, search_query: str):
+        # setting up a headless web driver to get search query
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless=new")
+        driver = webdriver.Chrome(options=options)
 
-    return html_page
+        url = AmazonScraper.__amazon_search_url + '+'.join(search_query.split())
+        driver.get(url)
+        driver.implicitly_wait(AmazonScraper.__wait_time)
 
-def get_closest_product_asin(html_page: str):
-    soup = BeautifulSoup(html_page, 'lxml')
+        html_page = driver.page_source
+        driver.quit()
 
-    # data-asin grabs products, while data-avar filters out sponsored ads
-    listings = soup.findAll('div', attrs={'data-asin': True, 'data-avar': False})
+        return html_page
 
-    asin_values = [single_listing['data-asin'] for single_listing in listings if len(single_listing['data-asin']) != 0]
+    def __get_closest_product_asin(self, html_page: str):
+        soup = BeautifulSoup(html_page, 'lxml')
 
-    return asin_values[0]
+        # data-asin grabs products, while data-avar filters out sponsored ads
+        listings = soup.findAll('div', attrs={'data-asin': True, 'data-avar': False})
 
-def __get_rated_reviews(url: str, num_reviews: int):
-    driver = webdriver.Chrome()
-    driver.get(url)
-    driver.implicitly_wait(3)
+        asin_values = [single_listing['data-asin'] for single_listing in listings if len(single_listing['data-asin']) != 0]
 
-    html_page = driver.page_source
-    driver.quit()
+        assert len(asin_values) > 0
 
-    soup = BeautifulSoup(html_page, 'lxml')
-    reviews = soup.findAll('span', attrs={'data-hook': True})
+        return asin_values[0]
 
-    reviews = [review for review in reviews if review['data-hook'] == 'review-body']
+    def __get_rated_reviews(self, url: str):
+        # setting up a headless web driver to get search query
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless=new")
+        driver = webdriver.Chrome(options=options)
 
-    # extract text from span tag and clean up newlines in string
-    reviews = [review.text.strip() for review in reviews]    
+        driver.get(url)
+        driver.implicitly_wait(AmazonScraper.__wait_time)
 
-    return reviews
+        html_page = driver.page_source
+        driver.quit()
 
-def __get_rated_reviews(url: str):
-    driver = webdriver.Chrome()
+        soup = BeautifulSoup(html_page, 'lxml')
+        html_reviews = soup.findAll('div', attrs={"data-hook": "review"})
 
-    # make browser headless so it works in the background
-    options = webdriver.ChromeOptions()
-    options.add_argument("headless")
-
-    driver.get(url)
-    driver.implicitly_wait(3)
-
-    html_page = driver.page_source
-    driver.quit()
-
-    soup = BeautifulSoup(html_page, 'lxml')
-    html_reviews = soup.findAll('div', attrs={"data-hook": "review"})
-
-    reviews = []
-    for html_review in html_reviews:
-        # extract text from various span tags and clean up newlines in their strings
-        name = html_review.find('span', class_='a-profile-name').text.strip()    
-
-        # Amazon's format is "x.0 stars out of 5" where x = # of stars
-        rating = html_review.find('span', class_='a-icon-alt').text.strip()[0]
-
-        review_body = html_review.find('span', attrs={'data-hook': 'review-body'}).text.strip()
-
-        reviews.append({'customer_name': name, 'rating': int(rating),'review': review_body})
-
-    return reviews
-
-def get_reviews(asin: str, num_reviews: int):
-    assert num_reviews % 5 == 0
-
-    base_url = AMAZON_REVIEW_URL + asin
-    overall_reviews = []
-
-    for star_num in range(1, 6):
-        url = base_url + star_page_suffix[star_num]
-
-        page_number = 1
         reviews = []
-        reviews_per_star = int(num_reviews / 5)
+        # extract text from various span tags and clean up newlines in their strings
+        for html_review in html_reviews:
+            name = html_review.find('span', class_='a-profile-name').text.strip()    
 
-        while len(reviews) <= reviews_per_star:
-            page_url = url + str(page_number)
-            print(page_url)
+            # Amazon's format is "x.0 stars out of 5" where x = # of stars
+            rating = html_review.find('span', class_='a-icon-alt').text.strip()[0]
 
-            # no reviews means we've exhausted all reviews
-            page_reviews = __get_rated_reviews(page_url)
+            review_body = html_review.find('span', attrs={'data-hook': 'review-body'}).text.strip()
 
-            if len(page_reviews) == 0:
-                break
+            reviews.append({'customer_name': name, 'rating': int(rating),'review': review_body})
 
-            reviews += page_reviews
-            page_number += 1
+        return reviews
 
-        # shave off extra reviews coming from the last page
-        reviews = reviews[:reviews_per_star]
-        overall_reviews += reviews
+    def __get_reviews(self, asin: str, num_reviews: int):
+        assert num_reviews % 5 == 0
 
-    return overall_reviews
+        base_url = AmazonScraper.__amazon_review_url + asin
+        overall_reviews = []
+
+        for star_num in range(1, 6):
+            url = base_url + AmazonScraper.__star_page_suffix[star_num]
+
+            page_number = 1
+            reviews = []
+            reviews_per_star = int(num_reviews / 5)
+
+            while len(reviews) <= reviews_per_star:
+                page_url = url + str(page_number)
+
+                # no reviews means we've exhausted all reviews
+                page_reviews = self.__get_rated_reviews(page_url)
+
+                if len(page_reviews) == 0:
+                    break
+
+                reviews += page_reviews
+                page_number += 1
+
+            # shave off extra reviews coming from the last page
+            reviews = reviews[:reviews_per_star]
+            overall_reviews += reviews
+
+        return overall_reviews
+
+    def get_closest_product_reviews(self, search_query):
+        html_page = self.__get_amazon_search_page(search_query)
+        product_asin = self.__get_closest_product_asin(html_page)
+        reviews = self.__get_reviews(asin = product_asin, num_reviews = 10)
+
+        return reviews
 
 if __name__ == "__main__":
-    search_query = 'iphone 15'
-    
-    html_page = get_amazon_search_page(search_query)
-    product_asin = get_closest_product_asin(html_page)
-    # print(product_asin)
+    # example use case
+    start = time.time()
 
-    reviews = get_reviews(asin = product_asin, num_reviews = 10)
+    search_query = 'iphone 15'
+    scraper = AmazonScraper()
+    reviews = scraper.get_closest_product_reviews(search_query)
+
+    end = time.time()
+
     print(reviews)
+    print(f"{end - start} seconds taken")
